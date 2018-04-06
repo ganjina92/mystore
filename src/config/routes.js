c25ae36acb94216237a5fabd335d6fa2ff7b32a2
Ganjina Sharifzoda <asu121416@gmail.com>
2018-04-06 10:09:47 -0600

    first commit

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import Cart from './components/cart/Cart'
 
 import ProductsList from './pages/ProductsList'
+import User from './pages/User'
 
 export default () => (
   <Switch>
-   <Route exact path="/" component={ProductsList}/>
-    <Route path="Cart" component={Cart}/>
+    <Route exact path="/" component={ProductsList}/>
+    <Route exact path="/users/:userID" component={User}/>
   </Switch>
 )
